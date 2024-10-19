b79349d7898694e1a3e11e9f4e405c9ee387959f
izzyLmao <rcht.1119@gmail.com>
2024-10-19 14:29:12 -0400

    test

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -11,17 +11,20 @@ function NotFound() {
 
   useEffect(() => {
     if (isDarkMode) {
-      document.body.classList.add("dark-mode");
+      document.body.classList.add("dark-mode"); // Add dark-mode class
     } else {
-      document.body.classList.remove("dark-mode");
+      document.body.classList.remove("dark-mode"); // Remove dark-mode class
     }
   }, [isDarkMode]);
 
   return (
     <Layout title={"IM SORRY UWU Page not found OWO かわいい <333"}>
       <h1>404</h1>
-      {/* Add the 'spin' class to make the image spin */}
-      <img src="victorLiMaid.png" alt="VictorUWU" className="spin" />
+      <img
+        src={isDarkMode ? "emoVictor.png" : "victorLiMaid.png"}
+        alt="VictorUWU"
+        className="spin"
+      />
       <h3>Toggle Dark/Light Mode:</h3>
       <button onClick={toggleDarkMode}>
         {isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
