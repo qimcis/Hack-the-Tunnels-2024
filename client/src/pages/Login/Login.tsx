cbd832fb40621299d55d1ae7b9cc6fe2a8954518
anushkatankala <anushkatankala@gmail.com>
2024-10-19 14:00:10 -0400

    top banner and title

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -34,7 +34,7 @@ function Login() {
           </div>
           {message && <p>{message}</p>}
           <div className="Login__panel__content__input">
-            <input type="text" placeholder="MyCarletonOne username"></input>
+            <input type="text" placeholder="MyVictorLiOne username"></input>
             <input type="password" placeholder="Password"></input>
           </div>
           <div className="Login__panel__content__checkbox">
